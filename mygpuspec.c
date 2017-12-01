0fda17443d2ae09f70c0f5aa54e2d39406fab9ee
David MacMahon <davidm@astro.berkeley.edu>
2017-11-30 23:42:04 -0800

    Add call to mygpuspec_copy_block_to_gpu()
    Also adding timing measurements of the transfers.  Can get over 12 GBps
    (gigaBYTES per second) transfer rate when runon same NUMA node as GPU.

--- mygpuspec.c
+++ mygpuspec.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <time.h>
 
 #include "mygpuspec.h"
 
+#define ELAPSED_NS(start,stop) \
+  (((int64_t)stop.tv_sec-start.tv_sec)*1000*1000*1000+(stop.tv_nsec-start.tv_nsec))
+
 int main(int argc, char * argv[])
 {
+  int i;
   mygpuspec_context ctx;
+
+  // Timing variables
+  struct timespec ts_start, ts_stop;
+  uint64_t elapsed_ns=0;
 
   int blocsize = 92274688;
 
@@ -21,8 +31,22 @@ int main(int argc, char * argv[])
     fprintf(stderr, "initialization failed\n");
     return 1;
   }
-
   printf("initialization succeeded\n");
+
+  clock_gettime(CLOCK_MONOTONIC, &ts_start);
+
+  for(i=0; i < ctx.Nb; i++) {
+    mygpuspec_copy_block_to_gpu(&ctx, i);
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &ts_stop);
+  elapsed_ns = ELAPSED_NS(ts_start, ts_stop);
+
+  printf("copied %u bytes in %.6f sec (%.3f GBps)\n",
+      blocsize * ctx.Nb,
+      elapsed_ns / 1e9,
+      blocsize * ctx.Nb / (double)elapsed_ns);
+
   printf("sleeping for 10 seconds...");
   fflush(stdout);
   sleep(10);
