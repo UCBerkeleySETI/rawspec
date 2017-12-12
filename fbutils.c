fa1d5ebfbf255ce2616d0f2952b8f8c0e20b0eb5
David MacMahon <davidm@astro.berkeley.edu>
2017-12-12 11:07:49 -0800

    Add initial version of fbutils.{c,h}

---
+++ fbutils.c
@@ -0,0 +1,261 @@
+// Routines for reading and writing filterbank headers from/to files and memory
+// buffers.  See fbutils.h for more details.
+
+#include <math.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <endian.h>
+
+#include "fbutils.h"
+
+double fb_ddd_to_dms(double ddd)
+{
+  int sign = ddd < 0 ? -1 : +1;
+  ddd = fabs(ddd);
+  double mm = 60*fmod(ddd, 1.0);
+  double ss = 60*fmod(mm,  1.0);
+  ddd = floor(ddd);
+  mm  = floor(mm);
+  return sign * (10000*ddd + 100*mm + ss);
+}
+
+ssize_t fb_fd_write_int(int fd, int32_t i)
+{
+  i = htole32(i);
+  return write(fd, &i, sizeof(int32_t));
+}
+
+void * fb_buf_write_int(void * buf, int32_t i)
+{
+  i = htole32(i);
+  *((int32_t *)buf) = i;
+  return buf + sizeof(int32_t);
+}
+
+ssize_t fb_fd_write_double(int fd, double d)
+{
+  uint64_t i = htole64(*(uint64_t *)&d);
+  return write(fd, &i, sizeof(uint64_t));
+}
+
+void * fb_buf_write_double(void * buf, double d)
+{
+  uint64_t i = htole64(*(uint64_t *)&d);
+  *((int64_t *)buf) = i;
+  return buf + sizeof(int64_t);
+}
+
+ssize_t fb_fd_write_angle(int fd, double d)
+{
+  return fb_fd_write_double(fd, fb_ddd_to_dms(d));
+}
+
+void * fb_buf_write_angle(void * buf, double d)
+{
+  return fb_buf_write_double(buf, fb_ddd_to_dms(d));
+}
+
+// Only write max 80 characters
+ssize_t fb_fd_write_string(int fd, const char * c)
+{
+  int32_t len = strnlen(c, 80);
+  fb_fd_write_int(fd, len);
+  return write(fd, c, len) + sizeof(int32_t);
+}
+
+// Only write max 80 characters
+void * fb_buf_write_string(void * buf, const char * c)
+{
+  int32_t len = strnlen(c, 80);
+  buf = fb_buf_write_int(buf, len);
+  memcpy(buf, c, len); 
+  return buf + len;
+}
+
+// TODO Add return value checking
+ssize_t fb_fd_write_header(int fd, const fb_hdr_t * hdr)
+{
+  ssize_t n;
+
+  n  = fb_fd_write_string(fd, "HEADER_START");
+  n += fb_fd_write_string(fd, "machine_id");
+  n += fb_fd_write_int(   fd, hdr->machine_id);
+  n += fb_fd_write_string(fd, "telescope_id");
+  n += fb_fd_write_int(   fd, hdr->telescope_id);
+  n += fb_fd_write_string(fd, "src_raj");
+  n += fb_fd_write_angle( fd, hdr->src_raj);
+  n += fb_fd_write_string(fd, "src_dej");
+  n += fb_fd_write_angle( fd, hdr->src_dej);
+  n += fb_fd_write_string(fd, "az_start");
+  n += fb_fd_write_double(fd, hdr->az_start);
+  n += fb_fd_write_string(fd, "za_start");
+  n += fb_fd_write_double(fd, hdr->za_start);
+  n += fb_fd_write_string(fd, "data_type");
+  n += fb_fd_write_int(   fd, hdr->data_type);
+  n += fb_fd_write_string(fd, "fch1");
+  n += fb_fd_write_double(fd, hdr->fch1);
+  n += fb_fd_write_string(fd, "foff");
+  n += fb_fd_write_double(fd, hdr->foff);
+  n += fb_fd_write_string(fd, "nchans");
+  n += fb_fd_write_int(   fd, hdr->nchans);
+  n += fb_fd_write_string(fd, "nbeams");
+  n += fb_fd_write_int(   fd, hdr->nbeams);
+  n += fb_fd_write_string(fd, "ibeam");
+  n += fb_fd_write_int(   fd, hdr->ibeam);
+  n += fb_fd_write_string(fd, "nbits");
+  n += fb_fd_write_int(   fd, hdr->nbits);
+  n += fb_fd_write_string(fd, "tstart");
+  n += fb_fd_write_double(fd, hdr->tstart);
+  n += fb_fd_write_string(fd, "tsamp");
+  n += fb_fd_write_double(fd, hdr->tsamp);
+  n += fb_fd_write_string(fd, "nifs");
+  n += fb_fd_write_int(   fd, hdr->nifs);
+  if(hdr->barycentric) {
+    n += fb_fd_write_string(fd, "barycentric");
+    n += fb_fd_write_int(   fd, hdr->barycentric);
+  }
+  if(hdr->pulsarcentric) {
+    n += fb_fd_write_string(fd, "pulsarcentric");
+    n += fb_fd_write_int(   fd, hdr->pulsarcentric);
+  }
+  n += fb_fd_write_string(fd, "source_name");
+  n += fb_fd_write_string(fd, hdr->source_name);
+  n += fb_fd_write_string(fd, "rawdatafile");
+  n += fb_fd_write_string(fd, hdr->rawdatafile);
+  n += fb_fd_write_string(fd, "HEADER_END");
+
+  return n;
+}
+
+void * fb_buf_write_header(void * buf, const fb_hdr_t * hdr)
+{
+  buf = fb_buf_write_string(buf, "HEADER_START");
+  buf = fb_buf_write_string(buf, "machine_id");
+  buf = fb_buf_write_int(   buf, hdr->machine_id);
+  buf = fb_buf_write_string(buf, "telescope_id");
+  buf = fb_buf_write_int(   buf, hdr->telescope_id);
+  buf = fb_buf_write_string(buf, "src_raj");
+  buf = fb_buf_write_angle( buf, hdr->src_raj);
+  buf = fb_buf_write_string(buf, "src_dej");
+  buf = fb_buf_write_angle( buf, hdr->src_dej);
+  buf = fb_buf_write_string(buf, "az_start");
+  buf = fb_buf_write_double(buf, hdr->az_start);
+  buf = fb_buf_write_string(buf, "za_start");
+  buf = fb_buf_write_double(buf, hdr->za_start);
+  buf = fb_buf_write_string(buf, "data_type");
+  buf = fb_buf_write_int(   buf, hdr->data_type);
+  buf = fb_buf_write_string(buf, "fch1");
+  buf = fb_buf_write_double(buf, hdr->fch1);
+  buf = fb_buf_write_string(buf, "foff");
+  buf = fb_buf_write_double(buf, hdr->foff);
+  buf = fb_buf_write_string(buf, "nchans");
+  buf = fb_buf_write_int(   buf, hdr->nchans);
+  buf = fb_buf_write_string(buf, "nbeams");
+  buf = fb_buf_write_int(   buf, hdr->nbeams);
+  buf = fb_buf_write_string(buf, "ibeam");
+  buf = fb_buf_write_int(   buf, hdr->ibeam);
+  buf = fb_buf_write_string(buf, "nbits");
+  buf = fb_buf_write_int(   buf, hdr->nbits);
+  buf = fb_buf_write_string(buf, "tstart");
+  buf = fb_buf_write_double(buf, hdr->tstart);
+  buf = fb_buf_write_string(buf, "tsamp");
+  buf = fb_buf_write_double(buf, hdr->tsamp);
+  buf = fb_buf_write_string(buf, "nifs");
+  buf = fb_buf_write_int(   buf, hdr->nifs);
+  if(hdr->barycentric) {
+    buf = fb_buf_write_string(buf, "barycentric");
+    buf = fb_buf_write_int(   buf, hdr->barycentric);
+  }
+  if(hdr->pulsarcentric) {
+    buf = fb_buf_write_string(buf, "pulsarcentric");
+    buf = fb_buf_write_int(   buf, hdr->pulsarcentric);
+  }
+  buf = fb_buf_write_string(buf, "source_name");
+  buf = fb_buf_write_string(buf, hdr->source_name);
+  buf = fb_buf_write_string(buf, "rawdatafile");
+  buf = fb_buf_write_string(buf, hdr->rawdatafile);
+  buf = fb_buf_write_string(buf, "HEADER_END");
+
+  return buf;
+}
+
+#ifdef FBUTILS_TEST
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char * argv[])
+{
+  fb_hdr_t hdr;
+
+  // 0=fake data; 1=Arecibo; 2=Ooty... others to be added
+  hdr.machine_id = 20; // wtf?
+  // 0=FAKE; 1=PSPM; 2=WAPP; 3=OOTY... others to be added
+  hdr.telescope_id = 6; // GBT
+  // 1=filterbank; 2=time series... others to be added
+  hdr.data_type = 1;
+  // 1 if barycentric or 0 otherwise (only output if non-zero)
+  hdr.barycentric = 1;
+  // 1 if pulsarcentric or 0 otherwise (only output if non-zero)
+  hdr.pulsarcentric = 1;
+  // right ascension (J2000) of source (hours)
+  // will be converted to/from hhmmss.s
+  hdr.src_raj = 20.0 + 39/60.0 + 7.4/3600.0;
+  // declination (J2000) of source (degrees)
+  // will be converted to/from ddmmss.s
+  hdr.src_dej = 42.0 + 24/60.0 + 24.5/3600.0;
+  // telescope azimuth at start of scan (degrees)
+  hdr.az_start = 12.3456;
+  // telescope zenith angle at start of scan (degrees)
+  hdr.za_start = 65.4321;
+  // centre frequency (MHz) of first filterbank channel
+  hdr.fch1 = 4626.464842353016138;
+  // filterbank channel bandwidth (MHz)
+  hdr.foff = -0.000002793967724;
+  // number of filterbank channels
+  hdr.nchans = 1;
+  // total number of beams
+  hdr.nbeams = 1;
+  // total number of beams
+  hdr.ibeam = 1;
+  // number of bits per time sample
+  hdr.nbits = 32;
+  // time stamp (MJD) of first sample
+  hdr.tstart = 57856.810798611114;
+  // time interval between samples (s)
+  hdr.tsamp = 1.825361100800;
+  // number of seperate IF channels
+  hdr.nifs = 1;
+  // the name of the source being observed by the telescope
+  // Max string size is supposed to be 80, but bug in sigproc if over 79
+  strcpy(hdr.source_name, "1234567890123456789012345678901234567890123456789012345678901234567890123456789");
+  // the name of the original data file
+  // Max string size is supposed to be 80, but bug in sigproc if over 79
+  strcpy(hdr.rawdatafile, "1234567890123456789012345678901234567890123456789012345678901234567890123456789");
+
+  float f = 0;
+
+  int fdfd  = open("fbutils_fd.fil",  O_WRONLY | O_CREAT, 0777);
+  int fdbuf = open("fbutils_buf.fil", O_WRONLY | O_CREAT, 0777);
+
+
+  ssize_t nbytes = fb_fd_write_header(fdfd, &hdr);
+  write(fdfd, (void *)&f, sizeof(float));
+  printf("write %lu+4 fd bytes\n", nbytes);
+
+  char buf[1024];
+  char * end = fb_buf_write_header(buf, &hdr);
+  nbytes = end-buf;
+  write(fdbuf, buf, nbytes);
+  write(fdbuf, (void *)&f, sizeof(float));
+  printf("write %lu+4 buf bytes\n", nbytes);
+
+  close(fdfd);
+  close(fdbuf);
+
+  return 0;
+}
+#endif // FBUTILS_TEST
---
+++ fbutils.h
@@ -0,0 +1,138 @@
+// Routines for reading and writing filterbank headers from/to files and memory
+// buffers.  A filterbank header consists of ASCII keywords that are preceeded
+// by a 4 byte integer specifying the ASCII keyword length.  Each keyword is
+// followed by 0 or more values, but usually just 1.  Each keyword has a
+// specific type of data that are expected.  For maximum compatibility with
+// historic implementions and currently dominant byte endianness, this library
+// outputs integer and double values in little endian format.
+//
+// The format of a filterbank header can be describled by this ASCII-gram:
+//
+//     IIIIHEADER_START
+//     IIIImachine_idIIII
+//     IIIItelescope_idIIII
+//     IIIIsrc_rajDDDDDDDD
+//     IIIIsrc_dejDDDDDDDD
+//     IIIIaz_startDDDDDDDD
+//     IIIIza_startDDDDDDDD
+//     IIIIdata_typeIIII
+//     IIIIfch1DDDDDDDD
+//     IIIIfoffDDDDDDDD
+//     IIIInchansIIII
+//     IIIInbeamsIIII
+//     IIIIibeamIIII
+//     IIIInbitsIIII
+//     IIIItstartDDDDDDDD
+//     IIIItsampDDDDDDDD
+//     IIIInifsIIII
+//     IIIIsource_nameIIII[C...]
+//     IIIIrawdatafileIIII[C...]
+//     IIIIHEADER_END
+//
+// Where IIII is a 4-byte integer, DDDDDDDD is an 8 byte double precision
+// floating point number, and [C...] is a variable length ASCII string (not NUL
+// terminated).  Additional keywords exist in the SIGPROC spec, but these are
+// the keywords currently supported by the routines in this file.
+//
+// Here is a sample output from the SIGPROC `header` program which reads a
+// filterbank header and outputs a human-readable version:
+//
+// Data file                        : blc34_guppi_57856_70053_DIAG_DR21_0034.gpuspec.0000.fil
+// Header size (bytes)              : 384
+// Data size (bytes)                : 805306368
+// Data type                        : filterbank (topocentric)
+// Telescope                        : GBT
+// Datataking Machine               : ?????
+// Source Name                      : DIAG_DR21
+// Source RA (J2000)                : 20:39:07.4
+// Source DEC (J2000)               : +42:24:24.5
+// Frequency of channel 1 (MHz)     : 4626.464842353016138
+// Channel bandwidth      (MHz)     : -0.000002793967724
+// Number of channels               : 67108864
+// Number of beams                  : 1
+// Beam number                      : 1
+// Time stamp of first sample (MJD) : 57856.810798611114
+// Gregorian date (YYYY/MM/DD)      : 2017/04/13
+// Sample time (us)                 : 18253611.00800
+// Number of samples                : 3
+// Observation length (seconds)     : 54.8
+// Number of bits per sample        : 32
+// Number of IFs                    : 1
+
+#ifndef _FBUTILS_H_
+#define _FBUTILS_H_
+
+#include <stdint.h>
+#include <sys/types.h>
+
+typedef struct {
+  // 0=fake data; 1=Arecibo; 2=Ooty... others to be added
+  int machine_id;
+  // 0=FAKE; 1=PSPM; 2=WAPP; 3=OOTY... others to be added
+  int telescope_id;
+  // 1=filterbank; 2=time series... others to be added
+  int data_type;
+  // 1 if barycentric or 0 otherwise (only output if non-zero)
+  int barycentric;
+  // 1 if pulsarcentric or 0 otherwise (only output if non-zero)
+  int pulsarcentric;
+  // right ascension (J2000) of source (hours)
+  // will be converted to/from hhmmss.s
+  double src_raj;
+  // declination (J2000) of source (degrees)
+  // will be converted to/from ddmmss.s
+  double src_dej;
+  // telescope azimuth at start of scan (degrees)
+  double az_start;
+  // telescope zenith angle at start of scan (degrees)
+  double za_start;
+  // centre frequency (MHz) of first filterbank channel
+  double fch1;
+  // filterbank channel bandwidth (MHz)
+  double foff;
+  // number of filterbank channels
+  int nchans;
+  // total number of beams
+  int nbeams;
+  // total number of beams
+  int ibeam;
+  // number of bits per time sample
+  int nbits;
+  // time stamp (MJD) of first sample
+  double tstart;
+  // time interval between samples (s)
+  double tsamp;
+  // number of seperate IF channels
+  int nifs;
+  // the name of the source being observed by the telescope
+  char source_name[81];
+  // the name of the original data file
+  char rawdatafile[81];
+} fb_hdr_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double fb_ddd_to_dms(double ddd);
+
+ssize_t fb_fd_write_int(int fd, int32_t i);
+void * fb_buf_write_int(void * buf, int32_t i);
+
+ssize_t fb_fd_write_double(int fd, double d);
+void * fb_buf_write_double(void * buf, double d);
+
+ssize_t fb_fd_write_angle(int fd, double d);
+void * fb_buf_write_angle(void * buf, double d);
+
+ssize_t fb_fd_write_string(int fd, const char * c);
+void * fb_buf_write_string(void * buf, const char * c);
+
+ssize_t fb_fd_write_header(int fd, const fb_hdr_t * hdr);
+void * fb_buf_write_header(void * buf, const fb_hdr_t * hdr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _FBUTILS_H_
