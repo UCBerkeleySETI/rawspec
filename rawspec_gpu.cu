e9a08b5626b8be87d16f185e456c4b6448a24dde
RocketRoss <code@radonn.co.za>
2021-02-10 13:32:34 +0000

    ^~ incoherent_sum fixed.

--- rawspec_gpu.cu
+++ rawspec_gpu.cu
@@ -277,25 +277,23 @@ __global__ void accumulate(float * pwr_buf, unsigned int Na, size_t xpitch, size
 }
 
 // Incoherent summation kernal (across antenna)
-__global__ void incoherent_sum(float * pwr_buf, float * incoh_buf, unsigned int Nant, size_t Nt, size_t Nc_coarse,
+__global__ void incoherent_sum(float * pwr_buf, float * incoh_buf, unsigned int Nant, size_t Nt,
                                 size_t ant_pitch, size_t chan_pitch, size_t pol_pitch, size_t spectra_pitch,
                                 size_t chan_out_pitch, size_t pol_out_pitch, size_t spectra_out_pitch
                               )
 {
 
-  const size_t fine_channel = blockIdx.z/Nc_coarse;
-  const bool lowerNotUpper = fine_channel == 0;//< (Nt+1)/2; 
   off_t offset_pwr = blockIdx.x * spectra_pitch
                 + blockIdx.y * pol_pitch
-                + blockIdx.z * chan_pitch + (lowerNotUpper ? 0 : (Nt+1)/2);
+                + blockIdx.z * chan_pitch + threadIdx.x;
   const off_t offset_ics = blockIdx.x * spectra_out_pitch
                 + blockIdx.y * pol_out_pitch
-                + (blockIdx.z * chan_out_pitch) + (lowerNotUpper ? (Nt+1)/2 : -spectra_out_pitch);
-  // if(lowerNotUpper)
-    for(unsigned int i=0; i<Nant; i++) {
-      incoh_buf[offset_ics] += pwr_buf[offset_pwr];
-      offset_pwr += ant_pitch;
-    }
+                + blockIdx.z * chan_out_pitch + threadIdx.x + (threadIdx.x < (Nt+1)/2 ? Nt/2 : -Nt/2);
+
+  for(unsigned int i=0; i<Nant; i++) {
+    incoh_buf[offset_ics] += pwr_buf[offset_pwr];
+    offset_pwr += ant_pitch;
+  }
 }
 
 // Stream callback function that is called right before an output product's GPU
@@ -1290,16 +1288,16 @@ int rawspec_start_processing(rawspec_context * ctx, int fft_dir)
       if(ctx->incoherently_sum){
         grid_ics.x = ctx->Nds[i];
         grid_ics.y = abs(ctx->Npolout[i]);
-        grid_ics.z = ctx->Nts[i]*ctx->Nc/ctx->Nant;
+        grid_ics.z = ctx->Nc/ctx->Nant;
 
-        incoherent_sum<<<grid_ics, 1>>>(gpu_ctx->d_pwr_out[i], gpu_ctx->d_ics_out[i],
-                                        ctx->Nant, ctx->Nts[i], ctx->Nc/ctx->Nant,
+        incoherent_sum<<<grid_ics, ctx->Nts[i]>>>(gpu_ctx->d_pwr_out[i], gpu_ctx->d_ics_out[i],
+                                        ctx->Nant, ctx->Nts[i],
                                         ctx->Nb*ctx->Ntpb*ctx->Nc/ctx->Nant, // Antenna pitch
-                                        ctx->Nb*ctx->Ntpb, // Channel pitch
+                                        ctx->Nb*ctx->Ntpb, // Coarse Channel pitch
                                         ctx->Nb*ctx->Ntpb*ctx->Nc, // Polarisation pitch
                                         ctx->Nts[i]*ctx->Nas[i], // Spectra pitch
                                         
-                                        ctx->Nts[i], // Channel pitch for ics
+                                        ctx->Nts[i], // Coarse Channel pitch for ics
                                         ctx->Nts[i]*ctx->Nc/ctx->Nant, // Polarisation pitch for ics
                                         abs(ctx->Npolout[i]) * ctx->Nts[i] * ctx->Nc/ctx->Nant // Spectra pitch for ics
                                         );
