6fe9dc62d810f5e0b4bbc5616168c2c5750094a7
Richard Elkins <richard.elkins@gmail.com>
2021-11-15 14:31:17 -0600

    Add fbh5_context_t, fbh5_ctx_ics and fbh5_ctx_ant definitions

--- rawspec_callback.h
+++ rawspec_callback.h
@@ -5,6 +5,20 @@
 #include "rawspec_fbutils.h"
 
 typedef struct {
+    hid_t file_id;              // File-level handle (similar to an fd)
+    hid_t dataset_id;           // Dataset "data" handle
+    hid_t dataspace_id;         // Dataspace handle for dataset "data"
+    unsigned int elem_size;     // Byte size of one spectra element (E.g. 4 if nbits=32)
+    hid_t elem_type;            // HDF5 type for all elements (derived from nbits in fbh5_open)
+    size_t tint_size;           // Size of a time integration (computed in fbh5_open)
+    hsize_t offset_dims[3];     // Next offset dimensions for the fbh5_write function
+                                // (offset_dims[0] : time integration count)
+    hsize_t filesz_dims[3];     // Accumulated file size in dimensions
+    unsigned long byte_count;   // Number of bytes output so far
+    unsigned long dump_count;   // Number of dumps processed so far
+} fbh5_context_t;
+
+typedef struct {
   int *fd; // Output file descriptors (one for each antenna) or socket (at most 1)
   int fd_ics; // Output file descriptor or socket
   unsigned int Nant; // Number of antenna, splitting Nf per fd
@@ -27,6 +41,11 @@ typedef struct {
   unsigned int Nf; // Number of fine channels (== Nc*Nts[i])
   // Filterbank header
   fb_hdr_t fb_hdr;
+  
+  // Added for FBH5 2021-11-15
+  fbh5_context_t fbh5_ctx_ics;    // Singleton fbh5 ctx for ics
+  fbh5_context_t * fbh5_ctx_ant;  // Pointer to array of fbh5 ctx for individual antennas
+
 } callback_data_t;
 
 #endif // _RAWSPEC_CALLBACK_H_
